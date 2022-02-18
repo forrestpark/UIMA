507ec57fac695474233f3ce9cd11dfbd9afe3046
Ray <rlpwns49@gmail.com>
2022-02-17 23:11:53 -0500

    finished calculate (except image sizing issue) and main

--- StressHits/app/src/main/java/com/uima/joanne/stresshits/CalculateActivity.java
+++ StressHits/app/src/main/java/com/uima/joanne/stresshits/CalculateActivity.java
@@ -1,10 +1,14 @@
 package com.uima.joanne.stresshits;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Pair;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CalculateActivity extends AppCompatActivity {
 
@@ -17,6 +21,7 @@ public class CalculateActivity extends AppCompatActivity {
     }
 
     public void onButtonClick(View view) {
+        Log.d("onButtonClick", "pressed!");
         EditText editStartHour = (EditText) findViewById(R.id.startHour);
         startHour = Integer.parseInt(editStartHour.getText().toString());
         EditText editStartMin = (EditText) findViewById(R.id.startMin);
@@ -29,16 +34,43 @@ public class CalculateActivity extends AppCompatActivity {
         if (isLegalHour(startHour) && isLegalMin(startMin) &&
             isLegalHour(endHour) && isLegalMin(endMin)) {
             // calculate time diff
+            Log.d("onButtonClick", "legal!");
             int[] result = calculateDifference(startHour, startMin, endHour, endMin);
-            diffHour, diffMin = result;
+
+            TextView diffHour = (TextView) findViewById(R.id.diffHour);
+            diffHour.setText(String.format("%02d", result[0]));
+
+            TextView diffMin = (TextView) findViewById(R.id.diffMin);
+            diffMin.setText(String.format("%02d", result[1]));
             //set difference
         } else {
             // toast!
+            Log.d("onButtonClick", "illegal!");
+            Context context = getApplicationContext();
+            CharSequence text = "Hello toast!";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
         }
     }
 
-    private Pair<Integer, Integer> calculateDifference(int startHour, int startMin, int endHour, int endMin) {
+    private int[] calculateDifference(int startHour, int startMin, int endHour, int endMin) {
+        int[] diff = new int[2];
 
+        if (endMin >= startMin) {
+            diff[1] = endMin - startMin;
+        } else {
+            endHour -= 1;
+            diff[1] = endMin + 60 - startMin;
+        }
+
+        if (endHour >= startHour) {
+            diff[0] = endHour - startHour;
+        } else {
+            diff[0] = endHour + 24 - startHour;
+        }
+
+        return diff;
     }
 
     private boolean isLegalMin(int min) {
