57a3ae391d0318cccdb8bcdc6f892a78b749b741
Ray Lee <rlpwns49@gmail.com>
2022-02-17 20:45:52 -0500

    temp work

--- StressHits/app/src/main/java/com/uima/joanne/stresshits/CalculateActivity.java
+++ StressHits/app/src/main/java/com/uima/joanne/stresshits/CalculateActivity.java
@@ -2,12 +2,13 @@ package com.uima.joanne.stresshits;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
 import android.widget.EditText;
 
 public class CalculateActivity extends AppCompatActivity {
 
-    private int startHour, startMin, endHour, endMin;
+    private int startHour, startMin, endHour, endMin, diffHour, diffMin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +20,25 @@ public class CalculateActivity extends AppCompatActivity {
         EditText editStartHour = (EditText) findViewById(R.id.startHour);
         startHour = Integer.parseInt(editStartHour.getText().toString());
         EditText editStartMin = (EditText) findViewById(R.id.startMin);
-        startHour = Integer.parseInt(editStartMin.getText().toString());
+        startMin = Integer.parseInt(editStartMin.getText().toString());
         EditText editEndHour = (EditText) findViewById(R.id.endHour);
-        startHour = Integer.parseInt(editEndHour.getText().toString());
+        endHour = Integer.parseInt(editEndHour.getText().toString());
         EditText editEndMin = (EditText) findViewById(R.id.endMin);
-        startHour = Integer.parseInt(editEndMin.getText().toString());
+        endMin = Integer.parseInt(editEndMin.getText().toString());
 
         if (isLegalHour(startHour) && isLegalMin(startMin) &&
             isLegalHour(endHour) && isLegalMin(endMin)) {
             // calculate time diff
+            int[] result = calculateDifference(startHour, startMin, endHour, endMin);
+            diffHour, diffMin = result;
+            //set difference
+        } else {
+            // toast!
         }
+    }
+
+    private Pair<Integer, Integer> calculateDifference(int startHour, int startMin, int endHour, int endMin) {
+
     }
 
     private boolean isLegalMin(int min) {
